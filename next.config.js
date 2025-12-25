d05e729e4ee90ebce0549a8a1de18d710e5e5581
ishanth <ishanthdev@gmail.com>
2025-12-25 13:05:43 +0530

    Redirect /login to /dashboard

---
+++ next.config.js
@@ -0,0 +1,19 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "/dashboard",
+        permanent: false,
+      },
+      {
+        source: "/signup",
+        destination: "/dashboard",
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
